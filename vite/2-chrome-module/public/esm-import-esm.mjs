048017141019a5a064caecc5728c9f35834d438f
houhongxu <hongxu.hou@goodwe.com>
2024-11-28 14:54:29 +0800

    feat: 补充chrome imort map

--- vite/2-chrome-module/public/esm-import-esm.mjs
+++ vite/2-chrome-module/public/esm-import-esm.mjs
@@ -3,3 +3,5 @@ import defaultEsm, { addEsm } from "./add-mjs.mjs";
 console.log("esm import esm", addEsm(1, 2));
 
 console.log("defaultEsm", defaultEsm);
+
+// 浏览器 esm 可以支持模块的相对路径导入导出
--- vite/2-chrome-module/public/react.mjs
+++ vite/2-chrome-module/public/react.mjs
@@ -1,2 +1,10 @@
 import React from "react";
-console.log(React);
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  React.createElement("div", null, "im react"),
+  document.getElementById("root")
+);
+
+// typemodule 也可以通过cdn引入esm库并执行
+// 但是想使用包名，需要通过浏览器的importmap
